389f95210a019fb9956d0e82168d49f26cf9954d
otis <tuannguyenvan1301@gmail.com>
2024-11-28 22:59:31 +0700

    rename and add title

--- wheelfinder/src/main/java/UI/WelcomeFace/StartWindow.java
+++ wheelfinder/src/main/java/UI/WelcomeFace/StartWindow.java
@@ -11,12 +11,12 @@ import javax.swing.JOptionPane;
  *
  * @author admin
  */
-public class Login extends javax.swing.JFrame {
+public class StartWindow extends javax.swing.JFrame {
 
     /**
      * Creates new form Login
      */
-    public Login() {
+    public StartWindow() {
         initComponents();
     }
 
@@ -84,6 +84,7 @@ public class Login extends javax.swing.JFrame {
         });
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Wheel Finder");
         setPreferredSize(new java.awt.Dimension(800, 500));
         setResizable(false);
 
@@ -629,20 +630,21 @@ public class Login extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(StartWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(StartWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(StartWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(StartWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                Login l = new Login();
+                StartWindow l = new StartWindow();
                 l.setVisible(true);
                 l.pack();
                 l.setLocationRelativeTo(null);
